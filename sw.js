203b30de72e4efe8f54ff2e14b19ae1a70053608
ptgame75-collab <ptgame75@gmail.com>
2026-01-14 00:43:08 +0545

    Upgrade service worker cache version and simplify code

--- sw.js
+++ sw.js
@@ -1,22 +1,10 @@
-const cacheName = 'adhiraj-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const cacheName = 'adhiraj-v2';
+const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
