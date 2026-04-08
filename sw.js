5eb108479f4154e939c7c24d09f2cbd2beb22e91
Aadhiraj sunar <ptgame75@gmail.com>
2026-04-08 12:32:31 +0545

    Update service worker for cache management
    Updated cache name to 'aadhiraj-v3' and added activation event to delete old caches. Modified fetch event to handle requests more effectively.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'aadhiraj-v2';
+const cacheName = 'aadhiraj-v3'; // अपडेट गर्दा यो नाम सधैं बदल्नुहोस्
 const assets = [
   './',
   './index.html',
@@ -8,11 +8,31 @@ const assets = [
   './new-logo.png'
 ];
 
+// इन्स्टल इभेन्ट
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting(); 
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
 });
 
+// एक्टिभेट इभेन्ट (पुरानो क्यास हटाउन)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// फेच इभेन्ट
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
-
